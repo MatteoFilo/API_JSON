a3c47ce79db20cfbc25497a5a54372e615994ca3
Matteo <matteo.filopanti@gmail.com>
2019-03-12 10:57:54 +0100

    First_Commit
    Impostazione per TEST JSON

---
+++ Server.js
@@ -0,0 +1,10 @@
+﻿const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => res.status(200).send({"message":"Return"}))
+
+
+app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
