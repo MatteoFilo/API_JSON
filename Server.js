6b9cf5718cddf78264122691b8c16009989db926
Matteo <matteo.filopanti@gmail.com>
2019-03-14 09:58:18 +0100

    FIX 11

--- Server.js
+++ Server.js
@@ -30,59 +30,16 @@ app.post("/", function(req,res){
 
   //res.json(req.body.queryResult.fulfillmentMessages.payload.user + " " + req.body.queryResult.fulfillmentMessages.payload.password + 1 );
   var resp = {
-  "fulfillmentText": "This is a text response",
+  "fulfillmentText": "this is fulfillmentText",
   "fulfillmentMessages": [
     {
-      "card": {
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-        "buttons": [
-          {
-            "text": "button text",
-            "postback": "https://assistant.google.com/"
-          }
-        ]
+      "simpleResponse": {
+        "text_to_speech": "ini simple responses",
+        "ssml": "ini ssml",
+        "display_text": "ini simple responses"
       }
     }
-  ],
-  "source": "example.com",
-  "payload": {
-    "google": {
-      "expectUserResponse": true,
-      "richResponse": {
-        "items": [
-          {
-            "simpleResponse": {
-              "textToSpeech": "this is a simple response"
-            }
-          }
-        ]
-      }
-    },
-    "facebook": {
-      "text": "Hello, Facebook!"
-    },
-    "slack": {
-      "text": "This is a text response for Slack."
-    }
-  },
-  "outputContexts": [
-    {
-      "name": "sss",
-      "lifespanCount": 5,
-      "parameters": {
-        "param": "param value"
-      }
-    }
-  ],
-  "followupEventInput": {
-    "name": "event name",
-    "languageCode": "en-US",
-    "parameters": {
-      "param": "param value"
-    }
-  }
+  ]
 }
     res.json(resp);
     //res.end("MATTEO");
