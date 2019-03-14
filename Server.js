d633e914f49805fc69f1646e025c79d3a2a3f74f
Matteo <matteo.filopanti@gmail.com>
2019-03-14 09:31:28 +0100

    FIX 6

--- Server.js
+++ Server.js
@@ -29,7 +29,8 @@ app.post("/", function(req,res){
   //req.body.Elemento = pass;
 
   //res.json(req.body.queryResult.fulfillmentMessages.payload.user + " " + req.body.queryResult.fulfillmentMessages.payload.password + 1 );
-    res.json(req.body);
+    //res.json(req.body);
+    res.end("MATTEO");
 });
 
 var port = process.env.PORT || 3000;
